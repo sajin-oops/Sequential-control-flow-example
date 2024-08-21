de35428816812d298ed58fe3064376573fcf0c95
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-21 23:53:37 +0530

    Add files via upload

---
+++ control flow.py
@@ -0,0 +1,19 @@
+# Sequential control flow example
+
+x = 5
+
+y = x * 2
+
+print("The value of y is:", y)
+
+y = y + 10
+
+print("The final value of y is:", y)
+
+'''
+O/P 
+
+The value of y is: 10
+The final value of y is: 20
+
+'''
